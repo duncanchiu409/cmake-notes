7e005f1b73512fb32119d77bd963d1cd303af898
duncanchiu409-sys <eu72@18m.dev>
2026-01-14 11:31:32 +0800

    Add: part4

---
+++ part4/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <part4Config.h>
+
+int main(int argc, char *argv[]) {
+  std::cout << "Hello World VERSION @ " << PART4_VERSION_MAJOR << "."
+            << PART4_VERSION_MINOR << std::endl;
+
+  return 0;
+}
